92d9c10594848de29e7e81886176a8dcb2ccb36d
EliseGAY <41110622+EliseGAY@users.noreply.github.com>
2023-10-19 11:09:33 +0200

    Add files via upload

---
+++ Variant_Calling_GATK/VC_all_genome/6_genotype_gvcf_all_Chr.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+#SBATCH --clusters=mesopsl1
+#SBATCH --account=gay
+#SBATCH --partition=def
+#SBATCH --qos=mesopsl1_def_long
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --job-name=genotyping
+#SBATCH --time=56:00:00
+#SBATCH -o genotyping.o
+#SBATCH -e genotyping.e
+
+# IMPORT MODULE
+module load gcc/9.2.0
+module load samtools/1.10
+module load gatk/4.2.0.0
+
+#==================#
+# Load Directories
+#==================#
+# don't know why but I can't manage to create variable with path and input / output. I have to write the path directly in the GATK command line
+
+# Just as a reminder, those are paths to load in the GATK command #
+# folder of DBImport/DB : (with three /// at the begining) 
+DB="///travail/egay/capture_analysis_GWS/Variant_Calling/GATK/DB_Import/DB/"
+
+# Temporary path
+Temp_path="/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/Temp/"
+
+# PATH to the reference genome fasta file (masked or not masked)
+Genome="/travail/egay/Genome_Reference/CarCar2.pri.cur.20210205.fasta"
+
+# interval list (to be mentioned even if you do your calling on all your chr)
+interval_list="/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/interval.list"
+
+Output="/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/VCF_214_samples_interval.vcf.gz"
+
+# create VCF folder outputs
+mkdir "/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/Temp/"
+
+#==================#
+# Run genotyping
+#==================#
+
+gatk --java-options "-Xmx100g" GenotypeGVCFs \
+-R "/travail/egay/Genome_Reference/CarCar2.pri.cur.20210205.fasta" \
+-V gendb:///travail/egay/capture_analysis_GWS/Variant_Calling/GATK/DB_Import/DB/ \
+-O "/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/VCF_214_samples_interval.vcf.gz" \
+--tmp-dir "/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/Temp/" \
+--include-non-variant-sites true \
+-L "/travail/egay/capture_analysis_GWS/Variant_Calling/GATK/VCF_214samples/interval.list"
