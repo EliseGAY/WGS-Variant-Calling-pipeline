b580ac8d0fb7dc5c21adb5707b24544e288aff29
Elise J Gay <41110622+EliseGAY@users.noreply.github.com>
2025-03-20 12:05:10 +0100

    Update Filter_Het.R

--- 6_DP_NA_Filters/R_functions_ploidy1/Filter_Het.R
+++ 6_DP_NA_Filters/R_functions_ploidy1/Filter_Het.R
@@ -1,61 +1,53 @@
-# ##############################################################
-# #### Filter Individues and Position by alternative rate #####
-# ##############################################################
+#' Filter Individuals and Positions by Alternative Rate
+#'
+#' This function filters a VCF file (Ploidy 1) based on the alternative allele rate both by individuals and by positions. 
+#' The function calculates the percentage of heterozygous (alternative) genotypes for each individual and for each position,
+#' and filters the VCF file according to the provided thresholds.
+#'
+#' @param vfc A VCF object (vcfR format) to be filtered.
+#' @param rate_het_max_POS (double) The maximum allowed alternative allele rate (percent) for positions.
+#' @param rate_het_max_Ind (double) The maximum allowed alternative allele rate (percent) for individuals.
+#' @param rate_het_min_pos (integer) The minimum number of alternative alleles allowed per position.
+#'
+#' @return A filtered VCF object where positions and individuals are removed based on the alternative allele rate filters.
+#' @import vcfR
+#' @export
+#'
+#' @examples
+#' # Filter a VCF file based on alternative rate thresholds
+#' filtered_vcf <- Filters_Het_Ind_Pos(vcf_file, 30, 50, 2)
+Filters_Het_Ind_Pos <- function(vfc, rate_het_max_POS, rate_het_max_Ind, rate_het_min_pos) {
+  
+  # Extract genotypes from the VCF
+  gt <- extract.gt(vfc, element = "GT", mask = FALSE, as.numeric = FALSE, return.alleles = FALSE, convertNA = FALSE, extract = TRUE)
 
-# Library
-#----------#
-library("vcfR")
+  # Calculate alternative allele rate for individuals (columns)
+  somme_het_ind <- colSums(gt == "1")
+  rate_het_ind <- (somme_het_ind / dim(gt)[1]) * 100
+  samples_kept <- names(rate_het_ind[rate_het_ind <= rate_het_max_Ind])
+  
+  # Filter VCF by individuals
+  vcf_filtered_ind <- vfc[, c("FORMAT", samples_kept)]
 
-# input
-# ----------#
-# A VCF file with Ploidy 1
+  # Extract genotypes for filtered VCF
+  gt_1 <- extract.gt(vcf_filtered_ind, element = "GT", mask = FALSE, as.numeric = FALSE, return.alleles = FALSE, convertNA = FALSE, extract = TRUE)
 
-# arg
-# -------#
-# rate_het_max_POS (double) : Max percent of alternative site in one position
-# rate_het_max_Ind (double) : Max percent of alternative site in one individus
-# rate_het_min_pos (int) : Min nb of alternative site in one position
-
-# output
-# -------#
-# VCF file filtered for alternative rate by Individu and then by position
-
-# Usage :
-# --------#
-# Filter_VCF=Filters_Het_Ind_Pos(vcf_file, rate_het_max_POS,rate_het_max_Ind)
-
-# Function 
-# -------------------------------------------------------------------------------------------------------------------------------------------#
-# Get genotype from vfc with the vcfR package
-# Count number of "1" per column and compute alternative rate (percent) by divided by the number of line (position)
-# Create new vcf with only samples which pass the filter
-# On the new VCF : Count number of "1" per line and compute alternative rate (percent) by divided by the number of column (individus)
-# Create new vcf with only samples AND position which pass the filter
-# -------------------------------------------------------------------------------------------------------------------------------------------#
-
-Filters_Het_Ind_Pos<-function(vfc, rate_het_max_POS,rate_het_max_Ind, rate_het_min_pos){
-	# Filters_Het_Ind_Pos(vcf_file, rate_het_max_POS,rate_het_max_Ind)
-	# Get genotypes data frame
-	gt<-extract.gt(vfc,element="GT",mask=F,as.numeric=F,return.alleles = F, convertNA = F,extract = T)
-	# get ind rate het (column)
-	somme_het_ind = colSums(gt=="1")
-	rate_het_ind = (somme_het_ind/dim(gt)[1])*100
-	samples_kept<-names(which(rate_het_ind<=rate_het_max_Ind))
-	vcf_filtered_ind<-vfc[,c("FORMAT",samples_kept)]
-
-	# get pos nb het min (lines)
-	gt_1<-extract.gt(vcf_filtered_ind,element="GT",mask=F,as.numeric=F,return.alleles = F, convertNA = F,extract = T)
-	somme_het_Pos = rowSums(gt_1=="1")
-	Pos_kept<-which(somme_het_Pos>=rate_het_min_pos)
-	vcf_filtered_Het_min<-vcf_filtered_ind[c(Pos_kept),]
-	
-	
-	# get pos rate het (lines)
-	gt_1<-extract.gt(vcf_filtered_Het_min,element="GT",mask=F,as.numeric=F,return.alleles = F, convertNA = F,extract = T)
-	somme_het_Pos = rowSums(gt_1=="1")
-	rate_het_Pos = (somme_het_Pos/dim(gt_1)[2])*100
-	Pos_kept<-which(rate_het_Pos<=rate_het_max_POS)
-	vcf_filtered_Het<-vcf_filtered_Het_min[c(Pos_kept),]
-	
-	return(vcf_filtered_Het)
+  # Calculate alternative allele count for positions (rows)
+  somme_het_pos <- rowSums(gt_1 == "1")
+  pos_kept <- which(somme_het_pos >= rate_het_min_pos)
+  
+  # Filter VCF by positions with enough alternative alleles
+  vcf_filtered_het_min <- vcf_filtered_ind[pos_kept, ]
+  
+  # Calculate alternative allele rate for positions (rows)
+  gt_1 <- extract.gt(vcf_filtered_het_min, element = "GT", mask = FALSE, as.numeric = FALSE, return.alleles = FALSE, convertNA = FALSE, extract = TRUE)
+  somme_het_pos <- rowSums(gt_1 == "1")
+  rate_het_pos <- (somme_het_pos / dim(gt_1)[2]) * 100
+  pos_kept <- which(rate_het_pos <= rate_het_max_POS)
+  
+  # Final filtered VCF by both position and individual heterozygosity rates
+  vcf_filtered_het <- vcf_filtered_het_min[pos_kept, ]
+  
+  # Return filtered VCF
+  return(vcf_filtered_het)
 }
