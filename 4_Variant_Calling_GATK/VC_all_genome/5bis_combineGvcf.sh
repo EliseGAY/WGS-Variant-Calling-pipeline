122f150a0acf0b60cea27a58f978fc6018cf37f5
Elise J Gay <41110622+EliseGAY@users.noreply.github.com>
2026-01-05 15:00:45 +0100

    Update 5bis_combineGvcf.sh

--- 4_Variant_Calling_GATK/VC_all_genome/5bis_combineGvcf.sh
+++ 4_Variant_Calling_GATK/VC_all_genome/5bis_combineGvcf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 #SBATCH -p std
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=16
 #SBATCH --job-name=gatk
-#SBATCH --time=30:00:00
-#SBATCH --mem-per-cpu=50G
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=200G
 #SBATCH -V
 
 # IMPORT MODULE
@@ -16,25 +16,12 @@
 chr_list=$(cat chr.list)
 
 # List of vcf file in the following format : --variant /PATH_TO_SAMPLE1/SAMPLE1_gatk.vcf.gz --variant  /PATH_TO_SAMPLE1/SAMPLE1_gatk.vcf.gz
-VCFs_Path="/path_to_vcfs/"
-Samples="sample1 sample2 sample3"
-Genome="/scratch/lasojada/Fourmis/ref_seq/flye_polished_yahs_curation_round2.1.primary.curated.fasta"
-DIRECTORY=$PWD
-
-# create variant vcf list formated for the Combine function
-VARIANTS=() # initialize the variant list
-for i in $Samples; do
-    file="$VCFs_Path/${i}.vcf.gz" # create full vcf path
-    [[ -f "$file" ]] && VARIANTS+=( "--variant" "$file" )
-done
-echo "${VARIANTS[@]}" # print variant list 
-
-mkdir All
-cd All
+VCFs_File="/path_to/VCF_list.txt"
+Genome="PATH_TO/Genome.fasta" # index .fai and .dict have to be present in the same directory (see samtools faidx and gatk CreateSequenceDictionary functions)
 
 # Run GATK
 Gatk --java -jar CombineGVCFs \
 -R ${Genome} \
-${ListOfFiles} \
-# -L $CHR \ : if you want to do that by chromosome
--O Cata_All.vcf.gz
+--variant ${VCFs_File} \
+# -L $CHR \ : if you want to do that by chromosome or on a subset
+-O All.vcf.gz
